052bae2f7054c02608758fc668e9bd8c094a09fc
IlhamNaffi-12210621 <naffiilham@gmail.com>
2024-12-02 01:25:32 +0700

    Upload

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
+    Person(String paramName){
+        this(paramName, null);
+    }
+
+    Person(){
+    }
+
+    void sayHello(String paramName){
+        System.out.println("Hello " + paramName + ", My Name Is, " + name);
+    }
+}
