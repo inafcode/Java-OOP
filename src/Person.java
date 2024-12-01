6b4c344775406d89e4fcc831596c26fa6a154d5a
IlhamNaffi-12210621 <naffiilham@gmail.com>
2024-12-02 01:36:50 +0700

    this_keyword

--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,14 @@ class Person {
     String address;
     final String country = "Indonesia";
 
-    Person(String paramName, String paramAddress){
-        name = paramName;
-        address = paramAddress;
+//    Person(String paramName, String paramAddress){
+//        name = paramName;
+//        address = paramAddress;
+//    }
+
+    Person(String name, String address){
+        this.name = name;
+        this.address = address;
     }
 
     Person(String paramName){
@@ -16,6 +21,6 @@ class Person {
     }
 
     void sayHello(String paramName){
-        System.out.println("Hello " + paramName + ", My Name Is, " + name);
+        System.out.println("Hello " + paramName + ", My Name Is, " + this.name);
     }
 }
